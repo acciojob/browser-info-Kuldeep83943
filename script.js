99869fb376dd859065200fb61bbc1e65446bd87d
Kuldeep83943 <134301330+Kuldeep83943@users.noreply.github.com>
2024-04-25 20:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let info = document.getElementById("browser-info");
+let b_name=navigator.appName
+let version=navigator.appVersion
+
+
+info.append("You are using "+b_name+" version "+version);
